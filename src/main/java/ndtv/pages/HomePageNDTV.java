0ea4eacaa3740e2d9b38ff6499c75ac546606782
chauhan_Parikshit <chauhanthe2nd@gmail.com>
2020-07-17 00:41:34 +0530

    reached Homepage

--- src/main/java/ndtv/pages/HomePageNDTV.java
+++ src/main/java/ndtv/pages/HomePageNDTV.java
@@ -1,7 +1,34 @@
 package ndtv.pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 import com.ndtv.BaseNDTV;
 
 public class HomePageNDTV extends BaseNDTV {
+	
+	@FindBy(xpath = "//a[@class='topnavmore']")
+	WebElement threeDotMenu;
+	
+	@FindBy(xpath = "//a[text()='WEATHER']")
+	WebElement weatherLink; 
+	
+	//Initializing Page objects (only relevant ones)
 
+	public HomePageNDTV() {
+		PageFactory.initElements(driver, this);
+	}
+	
+	
+	
+	public void OpenExtendedMenu() {
+		threeDotMenu.click();
+
+	}
+	
+	
+	
+	
+	
 }
--- src/test/java/com/weatherData/testcases/testClass.java
+++ src/test/java/com/weatherData/testcases/testClass.java
@@ -1,14 +1,32 @@
 package com.weatherData.testcases;
 
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import com.ndtv.BaseNDTV;
 
+import ndtv.pages.HomePageNDTV;
+//import pages.SeleniumTestPage;
+
+
+
 public class testClass extends BaseNDTV{
+	HomePageNDTV homepageNdtv;
+	
+	
+	
+	@BeforeMethod
+	public void SetupPage() {
+
+		homepageNdtv = new HomePageNDTV();
+	}
+	
+	
 	
 	
 	@Test
 	public void  verifyHomePageTitleTest() {
+		homepageNdtv.OpenExtendedMenu();
 		
 	}
 	
