ae34ee6f65c277ae1af87b972febc38cd6542748
chauhan_Parikshit <chauhanthe2nd@gmail.com>
2020-07-17 21:52:37 +0530

    working on Weather p[age - regular commit

--- src/main/java/ndtv/pages/WeatherPageNDTV.java
+++ src/main/java/ndtv/pages/WeatherPageNDTV.java
@@ -1,24 +1,35 @@
 package ndtv.pages;
 
+import javax.crypto.KeyAgreementSpi;
+
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import com.ndtv.BaseNDTV;
 
-public class WeatherPageNDTV extends BaseNDTV{
-	HomePageNDTV homepageNdtv;
-	WeatherPageNDTV weatherPageNdtv;
-	
-	
-	@BeforeMethod
-	public void SetupPage() {
-		Setup();
-		homepageNdtv = new HomePageNDTV();
-	}
-	
-	@Test
-	public void presenceOfCityInput() {
-		
+public class WeatherPageNDTV extends BaseNDTV {
+
+	// defining WebElements
+	@FindBy(xpath = "//input[@type='text' and @class  = 'searchBox']")
+	WebElement CityInputField;
+
+	public WeatherPageNDTV() {
+		PageFactory.initElements(driver, this);
 	}
 
+	
+	public boolean cityInputFieldEnabled() {
+		return CityInputField.isEnabled();
+	}
+	
+	public void enterCityName(String City) {
+		CityInputField.sendKeys(City, Keys.ENTER);
+		CityInputField.sendKeys(Keys.RETURN);
+	}
+	
+	
 }
--- src/test/java/com/weatherData/testcases/WeatherPageTest.java
+++ src/test/java/com/weatherData/testcases/WeatherPageTest.java
@@ -1,5 +1,6 @@
 package com.weatherData.testcases;
 
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -12,20 +13,23 @@ public class WeatherPageTest extends BaseNDTV {
 
 	HomePageNDTV homepageNdtv;
 	WeatherPageNDTV weatherPageNdtv;
-	
-	
+
 	@BeforeMethod
 	public void SetupPage() {
 		Setup();
 		homepageNdtv = new HomePageNDTV();
+		homepageNdtv.OpenExtendedMenu();
 		weatherPageNdtv = homepageNdtv.ClickWeatherLink();
 	}
-	
+
 	@Test
 	public void presenceOfCityInput() {
-		
+		Assert.assertEquals(weatherPageNdtv.cityInputFieldEnabled(), true);
 	}
 
+	@Test(dependsOnMethods = { "presenceOfCityInput" })
+	public void enterCityName() {
+		weatherPageNdtv.enterCityName("Lucknow");
+	}
 
-	
 }
