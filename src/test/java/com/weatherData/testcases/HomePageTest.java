3cbde685e145597f9756d88655b98c14e72bcf75
chauhan_Parikshit <chauhanthe2nd@gmail.com>
2020-07-17 19:57:36 +0530

    started homepagetest scripting

---
+++ src/test/java/com/weatherData/testcases/HomePageTest.java
@@ -0,0 +1,39 @@
+package com.weatherData.testcases;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.ndtv.BaseNDTV;
+
+import ndtv.pages.HomePageNDTV;
+//import pages.SeleniumTestPage;
+
+
+
+public class HomePageTest extends BaseNDTV{
+	HomePageNDTV homepageNdtv;
+	
+	
+	@BeforeMethod
+	public void SetupPage() {
+		homepageNdtv = new HomePageNDTV();
+	}
+	
+	
+	
+	
+	@Test
+	public void  verifyHomePageTitleTest() {  // to check if we have landed at the desired page
+		System.out.println(driver.getTitle());
+		homepageNdtv.OpenExtendedMenu();
+		Assert.assertEquals(driver.getTitle(), "NDTV: Latest News, India News, Breaking News, Business, Bollywood, Cricket, Videos & Photos");
+	}
+	
+	@Test
+	public void presenceOfWeatherLinkTest() {
+		
+	}
+
+	
+}
